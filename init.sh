593606e48b22192ff9ec9ea011da6125fdbbfe71
Matias Schertel <mschertel@gmail.com>
2016-11-11 04:18:51 +0000

    only create default beacon if no beacon exist

--- init.sh
+++ init.sh
@@ -3,17 +3,19 @@
 if [ -n "$BEACON" ]; then
 echo "$BEACON" > /data/beacon.js
 else
+if [ ! -f /data/beacon.js ]; then
 cat > /data/beacon.js <<- EOM
 var eddystoneBeacon = require('eddystone-beacon');
 var options = {
-  name: 'Eddy',      // set device name when advertising (Linux only)
+  name: 'Eddystone',      // set device name when advertising (Linux only)
   txPowerLevel: -22, // override TX Power Level, default value is -21,
   tlmCount: 2,       // 2 TLM frames
   tlmPeriod: 10      // every 10 advertisements
 };
-var url = 'https://github.com/';
+var url = 'https://docker.com/';
 eddystoneBeacon.advertiseUrl(url, [options]);
 EOM
+fi
 fi
 
 node /data/beacon.js
