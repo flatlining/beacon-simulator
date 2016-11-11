eb3a0e15146a743cf61bd12926bbe4823d46dbf3
Matias Schertel <mschertel@gmail.com>
2016-11-11 04:25:25 +0000

    add simple default beacon

--- init.sh
+++ init.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
 
 if [ -n "$BEACON" ]; then
-echo "$BEACON" > /data/beacon.js
+	echo "$BEACON" > /data/beacon.js
 else
-if [ ! -f /data/beacon.js ]; then
-cat > /data/beacon.js <<- EOM
-var eddystoneBeacon = require('eddystone-beacon');
-var options = {
-  name: 'Eddystone',      // set device name when advertising (Linux only)
-  txPowerLevel: -22, // override TX Power Level, default value is -21,
-  tlmCount: 2,       // 2 TLM frames
-  tlmPeriod: 10      // every 10 advertisements
-};
-var url = 'https://docker.com/';
-eddystoneBeacon.advertiseUrl(url, [options]);
-EOM
-fi
+	if [ ! -f /data/beacon.js ]; then
+		echo "require('eddystone-beacon').advertiseUrl('https://docker.com');" > /data/beacon.js
+	fi
 fi
 
 node /data/beacon.js
