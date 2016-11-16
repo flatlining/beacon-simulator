175348fbb941bcd628a9577e68b203305da349a8
Matias Schertel <mschertel@gmail.com>
2016-11-15 23:46:47 -0200

    add www folder

--- app/server.js
+++ app/server.js
@@ -3,11 +3,8 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
 // http://stackoverflow.com/a/35580597/3806928
+app.use('/', express.static(__dirname + '/www')); // redirect root
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
