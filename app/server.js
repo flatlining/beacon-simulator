cd189788e85b07d497a8a9b84e89b6ca596094ac
Matias Schertel <mschertel@gmail.com>
2016-11-15 23:39:08 -0200

    add local jquery and bootstrap

--- app/server.js
+++ app/server.js
@@ -1,10 +1,15 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+//app.use('/', express.static(__dirname + '/www')); // redirect root
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
